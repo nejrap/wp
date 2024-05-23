0699e91fe2d988d579d9a1b1785056f85f837165
Nejra <121252576+nejrap@users.noreply.github.com>
2024-05-23 22:00:05 +0200

    Add LoginSignup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
-import Gallery from './Pages/Gallery';
+
 import Shop from './Pages/Shop';
 
 import Footer from './Components/Footer/Footer';
@@ -21,10 +21,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/shop' element={<Shop/>}/>
-        <Route path="Gallery" element={<Gallery/>}>
-          <Route path=':galleryId' element={<Gallery/>}/>
-
-        </Route>
+      
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
 
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './CSS/LoginSignup.css'
 
 function LoginSignup(props) {
     return (
-        <div>
+        <div className='loginsignup'>
+            <div className='container'>
+                <h1>Registruj se</h1>
+                <div className='fields'>
+                    <input type='text' placeholder='Ime i prezime'/>
+                    <input type='email' placeholder='Email'/>
+                    <input type='password' placeholder='Password'/>
+
+                </div>
+                <button>Registruj se</button>
+                <p className='loginsignup-login'>Imaš account? <span> Prijavi se</span></p>
+
+            </div>
             
         </div>
     );
