d61af5d7329f3900cdb81768d3a551341a42f474
Nejra <121252576+nejrap@users.noreply.github.com>
2024-05-28 20:26:50 +0200

    update login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,82 +1,102 @@
+/* eslint-disable no-undef */
+/* eslint-disable default-case */
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
+import { useNavigate } from "react-router-dom";
 
-import React from 'react';
-import './CSS/LoginSignup.css'
-import { useRef } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-function Signup(props) {
-    let navigate = useNavigate('');
-    var ime = useRef('');
-    var prezime = useRef('');
-    var email = useRef('');
-    var username = useRef('');
-    var password = useRef('');
-    async function registrujSe() {
-        if(ime.current.value === "") return alert("Ime nije upisano.");
-    if(prezime.current.value === "") return alert("Prezime nije upisano.");
-    if(email.current.value === "") return alert("Email nije upisano.");
-    if(username.current.value === "") return alert("Username nije upisano.");
-    if(password.current.value === "") return alert("Password nije upisano.");
- 
-    const noviKorisnik = {
-        
-        FirstName: ime.current.value,
-        LastName: prezime.current.value,
-        Email: email.current.value,
-        PasswordHash: password.current.value,
-        Username: username.current.value
-    };
-   
+const INITIAL_USER_INFO = {
+  name: "",
+  lastName: "",
+  email: "",
+  userName: "",
+  password: "",
+};
+
+const Signup = (props) => {
+  const [userInfo, setUserInfo] = useState(INITIAL_USER_INFO);
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const onChangeHandler = (inputType, newValue) => {
+    setUserInfo((prev) => ({ ...prev, [inputType]: newValue }));
+  };
+
+  const validateForm = () => {
+    const { name, lastName, email, userName, password } = userInfo;
+    const errors = [];
+    if (!name) errors.push("No name");
+    if (!lastName) errors.push("No lastname");
+    if (!email) errors.push("No email");
+    if (!userName) errors.push("No username");
+    if (!password) errors.push("No password");
+
+    if (errors.length) alert(errors.join(", "));
+
+    setErrors(errors);
+  };
+
+  const registrujSe = async () => {
+    validateForm();
+    if (errors.length) return;
+
     await fetch(`https://localhost:7123/api/Login/dodajKorisnika`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then(() => {
+        setErrors([]);
+        setUserInfo(INITIAL_USER_INFO);
+        navigate(`/login`);
+      })
+      .catch((err) => alert(err));
+  };
 
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-
-        },
-
-         body: JSON.stringify(noviKorisnik),
-
-    });
- 
-    ime.current.value = "";
-
-    prezime.current.value = "";
-
-    email.current.value = "";
-
-    username.current.value = "";
-
-    password.current.value = "";
- 
-    navigate(`/login`);
-
-
-    }
-
-    
-    return (
-        <div className='loginsignup'>
-            <div className='container-login'>
-                <h1>Registruj se</h1>
-                <div className='fields'>
-                    <input type='text' placeholder='Ime' ref={ime}/>
-                    <input type='text' placeholder='Prezime' ref={prezime}/>
-                    <input type='email' placeholder='Email' ref={email}/>
-                    <input type='text' placeholder='username' ref={username}/>
-                    <input type='password' placeholder='Password' ref={password}/>
-
-                </div>
-                <button onClick={() => {registrujSe()}}>Registruj se</button>
-                <p className='loginsignup-login'>
-                Imaš account? <span onClick={() => navigate('/login')}>Prijavi se</span>
-                </p>
-
-            </div>
-            
+  return (
+    <div className="loginsignup">
+      <div className="container-login">
+        <h1>Registruj se</h1>
+        <div className="fields">
+          <input
+            type="text"
+            placeholder="Ime"
+            value={userInfo.name}
+            onChange={(e) => onChangeHandler("name", e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Prezime"
+            value={userInfo.lastName}
+            onChange={(e) => onChangeHandler("lastName", e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={userInfo.email}
+            onChange={(e) => onChangeHandler("email", e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            value={userInfo.userName}
+            onChange={(e) => onChangeHandler("userName", e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={userInfo.password}
+            onChange={(e) => onChangeHandler("password", e.target.value)}
+          />
         </div>
-    );
-}
+        <button onClick={registrujSe}>Registruj se</button>
+        <p className="loginsignup-login">
+          Imaš account? <span onClick={() => navigate("/login")}>Prijavi se</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
